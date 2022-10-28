7b947d5197017e1637919a236c99af2a452a343b
potistudio2022 <potistudio2022@gmail.com>
2022-10-28 21:31:04 +0900

    Hide the Token

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -9,10 +9,10 @@ document.getElementById ("windowCloseButton").addEventListener ("click", () => i
 let position = { lat: 0, lon: 0 };
 
 window.onload = async () => {
-	let geo = await fetch ("http://api.openweathermap.org/geo/1.0/direct?q=Tokyo,Japan&appid=3c48f42513dfd9f90a79ea4d2129d335");
+	let geo = await fetch ("http://api.openweathermap.org/geo/1.0/direct?q=Tokyo,Japan&appid=MYAPPTOKEN");
 	let geoJ = await geo.json();
 
-	let responce = await fetch ("https://api.openweathermap.org/data/3.0/onecall?lat=35.690&lon=139.692&appid=MYAPPID");
+	let responce = await fetch ("https://api.openweathermap.org/data/3.0/onecall?lat=35.690&lon=139.692&appid=MYAPPTOKEN");
 	let jsonData = await responce.json();
 
 
